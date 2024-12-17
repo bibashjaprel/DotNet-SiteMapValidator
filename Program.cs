f3975752a7c35213e356e33a06496ff7edfaddca
Niraj Paudel <itsmenirajpaudel@gmail.com>
2024-12-17 15:01:16 +1300

    Created a simple C# dotnet project for sitemap validation

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using Newtonsoft.Json;
+
+String URLString = "http://localhost:8080/v1/common/sitemap-deals";
+var reader = XmlReader.Create(URLString);
+
+while (reader.Read())
+{
+    if (reader.Name.Equals("loc"))
+    {
+        reader.Read();
+        if (reader.NodeType == XmlNodeType.Text)
+        {
+            Console.WriteLine(reader.Value);
+        }
+    }
+}
+
+public static class F
+{
+    public static string Dump(object obj)
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+}
